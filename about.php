f20a2fbe13507bd41cc4c3afd7dce854d869053a
Zerquix18 <malbertoa_11@hotmail.com>
2014-05-20 17:36:15 -0400

    Mejoras pocas...

--- about.php
+++ about.php
@@ -16,9 +16,9 @@ construir( 'cabecera', __('Acerca de'), false );
 <hr>
 <?php
 echo sprintf(
-		__('Bienvenido a <b>TrackYourPenguin %s</b>. TrackYourPenguin (<i>abreviado como TYP</i>) es un sistema de trackers en PHP y MySQL desarrollado actualmente por <a href="%s" target"_blank">Zerquix18</a>. El objetivo es que puedas tracker fácilmente con múltiples trackers, tuits, usuarios y otras cualidades sin la necesidad de tocar código constantemente o poner varios códigos para varios trackers. Todo en uno. :)'), constant('VERSION'), 'http://zerquix18.com/'
+		__('Bienvenido a <strong>TrackYourPenguin %s</strong>. TrackYourPenguin (<i>abreviado como TYP</i>) es un sistema de trackers en PHP y MySQL desarrollado actualmente por <a href="%s" target"_blank">Zerquix18</a>. El objetivo es que puedas tracker fácilmente con múltiples trackers, tuits, usuarios y otras cualidades sin la necesidad de tocar código constantemente o poner varios códigos para varios trackers. Todo en uno. :)'), constant('VERSION'), 'http://zerquix18.com/'
 	);
-echo sprintf( __('<h3>Novedades de la versión %s</h3><hr><ul><li>Cierre de sesión automático si alguien la cierra desde otro lado.</li><li>Cambios de tema desde la <a href="%s">configuración</a>.</li><li>Ahora los parámetros los coge el formulario a penas cliqueas.</li><li>Actualización en la tabla de <a href="%s">usuarios</a>.</li><li>Ésta página. :)</li></ul>'), constant("VERSION"), url() . 'ajustes.php', url() . 'usuarios.php');
-echo sprintf( __('<h3>Créditos</h3><ul><li><b>Bootstrap</b>. <i>Todo el diseño y plugins javascript del sitio</i>.</li><li><b>Bootswatch</b>. <i>Temas adicionales de bootstrap</i>.</Li><li><b>Zerquix18</b>. <i>Todo lo demás que ves aquí. :)</i></li></ul>') );
+echo sprintf( __('<h3>Novedades de la versión %s</h3><hr><ul><li>Cierre de sesión automático si alguien la cierra desde otro lado.</li><li>Cambios de tema desde la <a href="%s">configuración</a>.</li><li>Ahora los parámetros los coge el formulario a penas cliqueas.</li><li>Actualización en la tabla de <a href="%s">usuarios</a>.</li><li>Esta página. :)</li></ul>'), constant("VERSION"), url() . 'ajustes.php', url() . 'usuarios.php');
+echo sprintf( __('<h3>Créditos</h3><ul><li><strong>Bootstrap</strong>. <i>Todo el diseño y plugins javascript del sitio</i>.</li><li><strong>Bootswatch</strong>. <i>Temas adicionales de bootstrap</i>.</li><li><li><strong>Zerquix18</strong>. <i>Todo lo demás que ves aquí. :)</i></li></ul>') );
 ?>
 <?php construir('pies'); 
