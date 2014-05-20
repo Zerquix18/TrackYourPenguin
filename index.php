af4fb1d8f9584398fe3c8a175428150bef2263e0
Zerquix18 <malbertoa_11@hotmail.com>
2014-05-20 17:42:19 -0400

    Actualizado a ZerDB 1.0 y otras mejoras

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ require_once( dirname(__FILE__) . '/typ-load.php' );
 
 comprobar( false );
 
-$accion = ( isset($_GET['accion']) && !empty($_GET['accion']) && is_string($_GET['accion']) ) ? $_GET['accion'] : '';
+$accion = ( isset($_GET['accion']) && !empty($_GET['accion']) && is_string($_GET['accion']) ) ? $_GET['accion'] : ''; 
 $post = ( "POST" == $_SERVER['REQUEST_METHOD'] );
 switch( $accion ) {
 	case "agregar":
@@ -25,19 +25,19 @@ switch( $accion ) {
 	<h3><?php _e("Agregar tracker") ?></h3><a href="<?php echo url() ?>" class="btn btn-link pull-right"><?php _e("Volver") ?> &rarr;</a><hr>
 	<?php
 		if( $post ) {
-			$nombre = @$zerdb->proteger( $_POST['nombre'] );
-			$fuente = @$zerdb->proteger( $_POST['fuente'] );
+			$nombre = @$zerdb->real_escape( $_POST['nombre'] );
+			$fuente = @$zerdb->real_escape( $_POST['fuente'] );
 			$img = @$archivos->subir('img');
 
 			if( ! comprobar_args( @$_POST['nombre'], @$_POST['fuente'] ) ) {
-				agregar_error( __("Error en los datos HTML"), true, true);
+				typ_die( __("Haciendo trampa, ¿eh?") );
 			}elseif( vacios( $_POST['nombre'], $_POST['fuente'] ) ) {
 				agregar_error( __("No puedes dejar datos vacíos"), true, true);
-			}elseif( ! preg_match('/\./', $fuente ) ) {
+			}elseif( ! preg_match('-\.-', $fuente ) ) {
 				agregar_error( __("Estoy seguro de que la tipografía debe llevar un punto"), true, true);
 			}elseif( 'ttf' !== end( explode('.', $fuente) ) ) {
 				agregar_error( __("La fuente no tiene formato correcto"), true, true);
-			}elseif( ! file_exists( INC . strtolower($fuente) ) ) {
+			}elseif( ! file_exists( PATH . INC . strtolower($fuente) ) ) {
 				agregar_error( __("La fuente no existe en el directorio de incluidos"), true, true);
 			}elseif( $img->comp_error ) {
 				agregar_error( $img->error, true, true );
@@ -48,7 +48,7 @@ switch( $accion ) {
 			}
 		}
 	?>
-	<form method="POST" enctype="multipart/form-data" class="form-horizontal" action="<?php echo url( true ) ?>">
+	<form method="POST" enctype="multipart/form-data" class="form-horizontal" action="<?php echo url() . 'index.php?accion=agregar' ?>">
 		<div class="control-group">
 		<label class="control-label">
 			<?php _e("Nombre del tracker") ?>
@@ -81,10 +81,10 @@ switch( $accion ) {
 	if( ! es_admin() )
 		typ_die( __("¿Haciendo trampa, eh?") );
 
-	if( !isset($_GET['id']) || ! is_numeric($_GET['id']) )
+	if( !isset($_GET['id']) || ! is_numeric($_GET['id']) ) // si lleva una comilla, guión, lo que sea, is_numeric devolverá false ;)
 		typ_die( __("Debes especificar un id correcto") );
 
-	$t = obt_tracker( $zerdb->proteger($_GET['id']) );
+	$t = obt_tracker( $_GET['id'] );
 
 	if( true !== ($t && $t->nums > 0 ) )
 		typ_die( __("No existe ese tracker") );
@@ -92,11 +92,11 @@ switch( $accion ) {
 	construir('cabecera', sprintf( __('Editar tracker: %s'), ucwords($t->personaje) ) );
 
 	if( $post ) {
-		$nombre = @$zerdb->proteger( $_POST['personaje'] );
-		$fuente = @$zerdb->proteger( $_POST['fuente'] );
+		$nombre = @$zerdb->real_escape( $_POST['personaje'] );
+		$fuente = @$zerdb->real_escape( $_POST['fuente'] );
 
 		if( ! comprobar_args( @$_POST['personaje'], @$_POST['fuente']) ) {
-			agregar_error(__("Error en los datos HTML"));
+			typ_die(__("Haciendo trampa, ¿eh?"));
 		}elseif( vacios($_POST['personaje'], $_POST['fuente']) ) {
 			agregar_error(__("No puedes dejar campos vacíos"));
 		}elseif( ! preg_match('/\./', $fuente) ) {
@@ -104,7 +104,7 @@ switch( $accion ) {
 		}elseif( ! file_exists( INC . strtolower($fuente) ) ) {
 			agregar_error(__("El archivo de la fuente no existe en el directorio de incluidos") );
 		}else{
-			actualizar_tracker( $t->id, $nombre, $fuente );
+			$zerdb->update($zerdb->trackers, array("personaje" => $nombre, "fuente" => $fuente) )->where("id", $id)->_();
 			agregar_info(__("Tracker editado exitosamente"));
 			echo redireccion( url() . '?id=' . $t->id, 2);
 		}
@@ -143,23 +143,21 @@ case "eliminar":
 if( ! es_admin() )
 	typ_die(  __("Haciendo trampa, ¿eh?") );
 
-if( ! isset($_GET['ids'] ) || empty($_GET['ids']) || ! preg_match('/^[\d\,]+$/', @$_GET['ids']) ) /* Je, je, je, fail (?) */
-	typ_die( __("Mala forma de borrar, mala.") );
-
-$ids = explode(',', $_GET['ids']);
-
-$borrados = array();
-
-foreach( $ids as $id ) {
-	eliminar_tracker( $id );
-}
-
 construir('cabecera',  __('Eliminar trackers') );
 
+if( ! isset($_GET['ids'] ) || empty($_GET['ids']) || ! preg_match('/^[\d\,]+$/', @$_GET['ids']) ) /* Je, je, je, fail (?) */
+	typ_die( __("Mala forma de borrar, mala.") );
+$ids = explode(',', $_GET['ids']);
+foreach( $ids as $id ) {
+	$t = obt_tracker($id);
+	if( false == $t ) continue; // no existe? continuemos...
+	$archivos->eliminar( IMG . $t->imgbg ); 
+	$archivos->eliminar( IMG . $t->img );
+	$zerdb->delete($zerdb->trackers)->where('id', $id)->_();
+	$zerdb->delete($zerdb->parametros)->where('tracker', $id)->_();
+}
 agregar_info( __("Han sido eliminados los trackers seleccionados") );
-
 echo sprintf( __("<ul class=\"pager\"><li class=\"previous\"><a href=\"%s\">&larr;Volver</a></li></ul>"), url() . 'index.php' );
-
 break;
 case "ver":
 case "detalles":
@@ -169,21 +167,20 @@ $id = isset($_GET['id']) && is_numeric($_GET['id']) ? true : false;
 
 switch( $id ) {
 	case true:
-	$id = $zerdb->proteger( $_GET['id'] );
-	$t = obt_tracker( $id );
+	$t = obt_tracker( $_GET['id'] );
 
-	if( ! $t || !$t->nums > 0)
+	if( false == $t )
 		typ_die( __("El ID que especificas no existe") );
 
-	construir( 'cabecera', sprintf( __('Tracker de %s') , ucwords($t->personaje) ), true );
+	construir( 'cabecera', sprintf( __('Tracker de: %s') , ucwords($t->personaje) ), true );
 	?>
 	<h3><?php _e("Detalles del tracker") ?>: <i><?php echo ucwords( $t->personaje ) ?></i></h3><hr>
-	<p><b><?php _e("Nombre") ?></b>: <i><?php echo ucwords( $t->personaje ) ?></i></p>
-	<p><b><?php _e("Fuente") ?></b>: <i><?php echo strtolower( $t->fuente ) ?></i><?php echo (! file_exists(INC.$t->fuente) ) ? __('(no existe)') : '' ?></p>
+	<p><strong><?php _e("Nombre") ?></strong>: <i><?php echo ucwords( $t->personaje ) ?></i></p>
+	<p><strong><?php _e("Fuente") ?></strong>: <i><?php echo strtolower( $t->fuente ) ?></i><?php echo (! file_exists(INC.$t->fuente) ) ? __('(no existe)') : '' ?></p>
 	<?php if( es_admin() ): ?><a href="<?php echo url() . 'parametros.php?id=' . $t->id ?>" class="pull-right btn btn-link"><i class="icon-wrench"></i>&nbsp; <?php _e("Editar parámetros") ?></a><?php endif ?>
-	<hr><center><b><?php _e("Imagen de fondo") ?></b>:</center><br>
+	<hr><center><strong><?php _e("Imagen de fondo") ?></strong>:</center><br>
 	<center><img src="<?php echo url() . IMG . $t->imgbg ?>" title="<?php echo $t->imgbg ?>" alt="<?php echo $t->imgbg ?>" /></center>
-	<hr><center><b><?php _e("Imagen") ?></b>:</center><br>
+	<hr><center><strong><?php _e("Imagen") ?></strong>:</center><br>
 	<center><img src="<?php echo url() . IMG . $t->img ?>" title="<?php echo $t->img ?>" alt="<?php echo $t->img ?>" /></center><br>
 	<?php
 break;
@@ -192,31 +189,35 @@ break;
 
 	construir( 'cabecera', __('Trackers') );
 
-	$q = new extraer($zerdb->trackers, "*");
+	$q = $zerdb->select($zerdb->trackers)->_();
 
 	if( ! $q || !$q->nums > 0) {
-		agregar_error( sprintf( __("No tienes trackers :( <a href=\"%s\">¿Quieres agregar uno?</a> :)"), url() . '?accion=agregar'), false, false);
+		if( es_admin() )
+			agregar_error( sprintf( __("No tienes trackers ... <a href=\"%s\">¿Deseas agregar uno?</a> :)"), url() . '?accion=agregar'), false, false);
+		else
+			agregar_error( __('No existen trackers...  Notifíciale al administrador. :/') );
 		construir( 'pies' );
 		exit(0);
 	}
 	$pagina = isset($_GET['p']) && is_numeric($_GET['p']) ? $_GET['p'] : 1;
-	$limit = 'LIMIT '. ($pagina-1) * TRACKERS_PAG . ',' . TRACKERS_PAG;
-	$fetch = mysql_query( $q->query . ' ' . $limit );
-	$paginas = ceil( $q->nums / TRACKERS_PAG );
-	if( isset($_POST['eliminar']) && es_admin() ) {
+	$limit = 'LIMIT '. ($pagina-1) * 5 . ',' . 5;
+	$fetch = $zerdb->query( $zerdb->query . ' ' . $limit );
+	$paginas = ceil( $q->nums / 5 );
+	if( isset($_POST['eliminar']) ) {
 		if( ! isset($_POST['ids'] ) ) {
 			agregar_error( __("Debes seleccionar un tracker"), true, true);
 		}elseif( ! is_array($_POST['ids']) || empty($_POST['ids']) ) {
 			agregar_error( __("Debes seleccionar al menos uno"), true, true);
 		}else{
 			$nums = count($_POST['ids']);
-			$singular = __("¿Seguro que quieres eliminar %s tracker? <hr> <a href=\"%s\" class=\"btn btn-danger\">Sí</a>");
-			$plural = __("¿Seguro que quieres eliminar %s trackers? <hr> <a href=\"%s\" class=\"btn btn-danger\">Sí</a>");
+			$singular = __("¿Seguro que quieres eliminar %s tracker?");
+			$plural = __("¿Seguro que quieres eliminar %s trackers?");
 			$eliminar_url =	url() . '?accion=eliminar&ids=' . implode(',', $_POST['ids']);
-			agregar_info(
-					sprintf( _n($singular, $plural, $nums),
-					$nums, $eliminar_url),
-					false, true
+			echo sprintf(
+				"<script type=\"text/javascript\">window.onload = function() { cambiar_modal('%s', '%s', '%s', {'abrir' :  true} );  };</script>",
+					__('Confirmación'),
+					sprintf( _n($singular, $plural, $nums), $nums ),
+					'<a class="btn btn-danger" href="'. $eliminar_url .  '">Eliminar</a> | <button class="btn" data-dismiss="modal" aria-hidden="true">Cerrar</button>'
 				);
 		}
 	}
@@ -225,31 +226,29 @@ break;
 	<a href="<?php echo url() ?>index.php?accion=agregar" class="pull-right btn btn-link"><i class="icon-plus"></i>&nbsp;<?php _e("Agregar nuevo") ?></a><br>
 		<?php endif ?><center>
 	<?php
-	while($q->fetch = mysql_fetch_array($fetch) ) {
-		if( es_admin() ) :
+	while( $q = $fetch->r->fetch_object() ) {
+		if( es_admin() ) : // can't delete, bro ;)
 		echo sprintf(
-					"<input type=\"checkbox\" name=\"ids[]\" value=\"%d\">&nbsp;",
-				(int) $q->fetch['id']
+					"<input type=\"checkbox\" name=\"ids[]\" value=\"%s\">&nbsp;", $q->id
 			);
 		endif;
 		/***********************/
 		echo sprintf(
 					"<a href=\"%s\">%s</a>" . "\n\n",
-				url() . '?id=' . $q->fetch['id'],
-			ucwords($q->fetch['personaje'])
+				url() . '?id=' . $q->id,
+			ucwords($q->personaje)
 		);
 		/*********************************/
 		echo sprintf(
 					"<img src=\"%s\" class=\"thumbnail\" title=\"%s\" />\n\n",
-				url() . IMG . $q->fetch['img'],
-			__('Imagen de ') . ucwords( $q->fetch['personaje'])
+				url() . IMG . $q->img,
+			__('Imagen de ') . ucwords( $q->personaje)
 		);
 ?>
-	<center>
-		<a href="<?php echo url() . 'actualizar.php?id=' . $q->fetch['id'] ?>" class="btn btn-success" title="<?php echo sprintf( __("Actualizar el tracker de %s"), ucwords($q->fetch['personaje'] ) ) ?>"><i class="icon-pencil"></i>&nbsp;<?php _e("Actualizar") ?></a> |
-		<a href="<?php echo url() . '?id=' . $q->fetch['id'] ?>" title="<?php echo sprintf( __('Ver detalles del tracker de %s'), ucwords( $q->fetch['personaje']) ); ?>" class="btn btn-primary"><i class="icon-eye-open"></i>&nbsp;<?php _e("Detalles")?></a>
+		<a href="<?php echo url() . 'actualizar.php?id=' . $q->id ?>" class="btn btn-success" title="<?php echo sprintf( __("Actualizar el tracker de %s"), ucwords($q->personaje) ) ?>"><i class="icon-pencil"></i>&nbsp;<?php _e("Actualizar") ?></a> |
+		<a href="<?php echo url() . '?id=' . $q->id ?>" title="<?php echo sprintf( __('Ver detalles del tracker de %s'), ucwords( $q->personaje) ); ?>" class="btn btn-primary"><i class="icon-eye-open"></i>&nbsp;<?php _e("Detalles")?></a>
 		<?php if( es_admin() ) : ?>
-		| <a href="<?php echo url() . '?accion=editar&id=' . $q->fetch['id'] ?>" title="<?php echo sprintf( __('Editar el tracker de %s'), ucwords($q->fetch['personaje'] ) )?>" class="btn btn-warning"><i class="icon-edit"></i>&nbsp;<?php _e("Editar")?></a>
+		| <a href="<?php echo url() . '?accion=editar&id=' . $q->id ?>" title="<?php echo sprintf( __('Editar el tracker de %s'), ucwords($q->personaje) )?>" class="btn btn-warning"><i class="icon-edit"></i>&nbsp;<?php _e("Editar")?></a>
 		<?php endif ?>
 		<hr><br>
 <?php
@@ -262,7 +261,7 @@ break;
 	?>
 <div class="pagination pagination-large">
 	<ul>
-		<?php 
+		<?php
 		$n2 = ($pagina == '1') ? '1' : $pagina - 1;
 		echo '<li><a href="?p='. $n2 . '">&laquo;</a></li>';
 		for($i = 1; $i <= $paginas; $i++) :
