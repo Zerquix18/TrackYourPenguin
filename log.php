236d463db446d46baa769d89b332c60e5c2106b3
Zerquix18 <malbertoa_11@hotmail.com>
2014-05-20 17:44:08 -0400

    Actualizado a ZerDB 1.0 y cambio de <b> a </strong>

--- log.php
+++ log.php
@@ -18,12 +18,13 @@ if( ! es_super_admin() )
 construir('cabecera', __('Registro de cambios'), true );
 
 if( isset($_GET['id']) && is_numeric($_GET['id'] ) )
-	$dnd = array("id" => $zerdb->proteger($_GET['id'] ) );
+	$dnd = array("id" => $zerdb->real_escape($_GET['id'] ) );
 else
 	$dnd = false;
 
 $order = isset($_GET['id']) && is_numeric($_GET['id']) ? '' : "ORDER BY id DESC";
-$l = new extraer( $zerdb->log, "*", $dnd, $order );
+$l = $zerdb->select( $zerdb->log, "*", $dnd)->add($order);
+$l = $l->execute();
 
 if( isset($_GET['id']) && is_numeric($_GET['id'] ) ) {
 	if( ! $l || !$l->nums > 0 ) {
@@ -35,7 +36,7 @@ if( isset($_GET['id']) && is_numeric($_GET['id'] ) ) {
 	<div class="well">
 		<?php echo mostrar_log($l->accion) ?>
 	</div>
-	<b><?php _e("Fecha") ?>:</b>&nbsp;<i><?php echo mostrar_fecha($l->fecha, true) ?></i>
+	<strong><?php _e("Fecha") ?>:</strong>&nbsp;<i><?php echo mostrar_fecha($l->fecha, true) ?></i>
 	<ul class="pager">
 		<li class="previous"><a href="<?php echo url() . 'log.php' ?>">&larr; <?php _e("Volver al log") ?></a></li>
 	</ul>
@@ -46,12 +47,12 @@ if( isset($_GET['id']) && is_numeric($_GET['id'] ) ) {
 <h3> <?php _e("Registro de cambios") ?> </h3><hr>
 <?php
 	if( !$l || $l->nums == "0") {
-		agregar_error( __("No se ha registrado nada aún en el registro, vuelve más tarde. :)") );
+		agregar_error( __("No se ha registrado nada aún en el registro o los registros han sido borrados.") );
 		exit( construir('pies') );
 	}
 	$pagina = isset($_GET['p']) && is_numeric($_GET['p']) ? $_GET['p'] : 1;
 	$limit = 'LIMIT '. ($pagina-1) * 5 . ',' . 5;
-	$fetch = mysql_query( $l->query . ' ' . $limit );
+	$fetch = $zerdb->query( $zerdb->query . ' ' . $limit );
 	$paginas = ceil( $l->nums / 5 );
 if( $pagina > $paginas ) {
 	agregar_error( __("No existen más registros") );
@@ -65,13 +66,13 @@ if( $pagina > $paginas ) {
 		agregar_error( __("Debes seleccionar un log para borrar") );
 	}else{
 		foreach( $_POST['eliminar'] as $id ) {
-			$zerdb->eliminar( $zerdb->log, array("id" => $zerdb->proteger($id) ) );
+			$zerdb->delete( $zerdb->log, array("id" => $zerdb->real_escape($id) ) )->_();
 		}
 		agregar_info( __("Los registros seleccionados han sido eliminados") );
 		echo redireccion( url( true ), 2);
 	}
 }elseif( isset($_POST['eliminar_todo'] ) ) {
-	$zerdb->eliminar($zerdb->log );
+	$zerdb->delete($zerdb->log )->_();
 	echo redireccion( url( true ), 0);
 }
 ?>
@@ -82,7 +83,7 @@ if( $pagina > $paginas ) {
 		<th><?php _e("Fecha") ?></th>
 	</tr>
 <?php
-while($log = mysql_fetch_array($fetch) ) {
+while($log = $fetch->r->fetch_array() ) {
 	?>
 	<tr>
 		<td><input type="checkbox" name="eliminar[]" value="<?php echo $log['id'] ?>"></td>
@@ -91,7 +92,7 @@ while($log = mysql_fetch_array($fetch) ) {
 					echo substr( mostrar_log($log['accion']), 0, 100) . '...<a href="' . url() . 'log.php?id=' . $log['id'] . '">' . __('más') . '</a>';
 				else echo mostrar_log($log['accion']);
 		?></td>
-		<td><?php echo '<b>' . mostrar_fecha($log['fecha'])  . '</b>' ?></td>
+		<td><?php echo '<strong>' . mostrar_fecha($log['fecha'])  . '</strong>' ?></td>
 	</tr>
 <?php } ?>
 </table>
@@ -117,5 +118,4 @@ while($log = mysql_fetch_array($fetch) ) {
 		<i class="icon-trash"></i>&nbsp;<?php _e("Limpiar elementos seleccionados") ?>
 	</button>
 </form>
-
 <?php construir('pies') ?>
