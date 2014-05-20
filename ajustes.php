73584df3618413c8205f4fe8027f6ad191abd6b3
Zerquix18 <malbertoa_11@hotmail.com>
2014-05-20 17:41:05 -0400

    No más configuración de robots

--- ajustes.php
+++ ajustes.php
@@ -19,23 +19,22 @@ if( ! es_super_admin() )
 construir( 'cabecera', __('Ajustes'), true );
 
 $post = 'POST' == getenv('REQUEST_METHOD');
-$c = new extraer($zerdb->config, "*");
+$c = $zerdb->select($zerdb->config, "*")->_();
 $t_ = json_decode($c->extra)->tema;
 ?>
 <h3><?php _e("Ajustes") ?></h3>
 <?php
 if( $post ) {
-	$nombre = @$zerdb->proteger( $_POST['nombre'] );
-	$url = isset($_POST['url']) ? @$zerdb->proteger($_POST['url']) : url( false );
-	$robots = isset($_POST['robots']) ? '1' : '0';
+	$nombre = @$zerdb->real_escape( $_POST['nombre'] );
+	$url = isset($_POST['url']) ? @$zerdb->real_escape($_POST['url']) : url( false );
 	$url = preg_replace('/[\/]$/', '', $url); // quita el slash final
 	$url = preg_replace('/^(https)/', 'http', $url); // reemplaza https por http
-	if( ! preg_match('/^[http]/',  $url) ) //si.. no empieza por http...
+	if( ! preg_match('/^(http)/',  $url) ) //si.. no empieza por http... adds it. c:
 		$url = 'http://' . $url;
 	$temas = array("bootstrap", "cyborg", "slate", "flatly", "cosmo", "cerulean");
 	$tema = comprobar_args( @$_POST['tema']) && in_array($_POST['tema'], $temas) ? $_POST['tema'] : 'bootstrap';
 	if( ! comprobar_args( @$_POST['nombre'], @$_POST['url'] ) ) {
-		agregar_error( __("Haciendo trampa, ¿eh?"), true, true);
+		typ_die( __("Haciendo trampa, ¿eh?") );
 	}elseif( vacios($_POST['url'], $_POST['nombre'] ) ) {
 		agregar_error( __("No puedes dejar campos vacíos"), true, true);
 	}elseif( strlen($nombre) > 20 ) {
@@ -43,9 +42,9 @@ if( $post ) {
 	}elseif( ! filter_var($url, FILTER_VALIDATE_URL) ) {
 		agregar_error( __("El URL no parece ser válido"), true, true);
 	}else{
-		$zerdb->actualizar( $zerdb->config, 
-				array("titulo" => $nombre, "url" => $url, "robots" => $robots, "extra" => json_encode( array("tema" => $tema) ) )
-			);
+		$zerdb->update( $zerdb->config, 
+				array("titulo" => $nombre, "url" => $url, "extra" => json_encode( array("tema" => $tema) ) )
+			)->_();
 		agregar_info( __("La configuración ha sido actualizada") );
 		echo redireccion( url( true ) , 2);
 	}
@@ -73,15 +72,6 @@ if( $post ) {
 	</div>
 	<div class="control-group">
 		<label class="control-label">
-			<?php _e("Buscadores") ?>
-		</label>
-		<div class="controls">
-			<input type="checkbox" name="robots" <?php if( '1' == $c->robots ) echo 'checked="checked"' ?>>
-			<span class="help-inline"><?php _e("¿Pueden los buscadores indexar esta administración?") ?></span>
-		</div>
-	</div>
-	<div class="control-group">
-		<label class="control-label">
 			<?php _e('Tema') ?>
 		</label>
 		<div class="controls">
@@ -93,9 +83,8 @@ if( $post ) {
 				<option <?php if( "cosmo" == $t_ ) echo 'selected="selected"' ?> value="cosmo">Cosmo</option>
 				<option <?php if( "cerulean" == $t_ ) echo 'selected="selected"' ?> value="cerulean">Cerulean</option>
 			</select>
-			<span class="help-inline"><?php _e('¿Qué tema te gusta más?') ?></span>
+			<span class="help-block"><?php _e('¿Qué tema te gusta más?') ?></span>
 		</div>
 	</div>
 	<hr><center><input type="submit" name="enviar" value="<?php _e('Actualizar') ?>" id="enviar" class="btn btn-primary"></center>
-<?php
-construir('pies');
+<?php construir('pies');
