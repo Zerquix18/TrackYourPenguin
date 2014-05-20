61219d5a4c7ec260482474174a2355a8ca7fb4bc
Zerquix18 <malbertoa_11@hotmail.com>
2014-05-20 17:43:23 -0400

    Cambio de <b> a <strong>

--- instalar.php
+++ instalar.php
@@ -1,5 +1,5 @@
 <?php
-/** 
+/**
 * Archivo de instalación de TrackYourPenguin
 *
 * @author Zerquix18
@@ -23,8 +23,8 @@ function construir_cabecera() {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="<?php echo  INC . CSS . 'cyborg.css' ?>" rel="stylesheet">
   <style type="text/css">
-    body { padding-top: 60px; 
-      padding-bottom: 40px; 
+    body { padding-top: 60px;
+      padding-bottom: 40px;
     }
   </style>
   <link href="<?php echo INC . CSS . 'cyborg.min.css' ?>" rel="stylesheet">
@@ -42,23 +42,23 @@ function construir_pies() { global $v;
 </div>
 	    <hr>
     <footer>
-      <p>&copy; <a href="//trackyourpenguin.com" target="_blank">TrackYourPenguin, <?php _e("versión") ?>&nbsp;<b><?php echo $v ?></b></a></p>
+      <p>&copy; <a href="//trackyourpenguin.com" target="_blank">TrackYourPenguin, <?php _e("versión") ?>&nbsp;<strong><?php echo $v ?></strong></a></p>
     </footer>
 
-  </div> 
+  </div>
 </body>
 </html>
 	<?php
 }
 
 function error_renombrar() {
-  echo __('Hubo un error renombrando el archivo, al parecer tu hosting no lo permite. Por favor, renómbralo de "<b>typ-config-sample.php</b>"  a "<b>typ-config.php</b>"');
+  echo __('Hubo un error renombrando el archivo, al parecer tu hosting no lo permite. Por favor, renómbralo de "<strong>typ-config-sample.php</strong>"  a "<strong>typ-config.php</strong>"');
 }
 
 /* ¿Ya hemos instalado */
 function comp() {
   if( comprobar_instalacion() ) {
-    agregar_error( __("<h2><b>Ya has instalado</b></h2><hr> La instalación de TrackYourPenguin ya fue completada, por lo que, ¿no piensas hacerla de nuevo, verdad?"), false, false);
+    agregar_error( __("<h2><strong>Ya has instalado</strong></h2><hr> La instalación de TrackYourPenguin ya fue completada, por lo que, ¿no piensas hacerla de nuevo, verdad?"), false, false);
     construir_pies();
     exit();
   }
@@ -72,7 +72,7 @@ switch( $paso ) {
 	if( !file_exists( PATH . $file2 ) ) :  //si no existe typ-config.php
     require_once("./" . $file);
   $test = new zerdb(DB_HOST, DB_USUARIO, DB_CLAVE, DB_NOMBRE);
-  if( $test->listo ) :
+  if( $test->ready ) :
     if( @rename( PATH . $file, PATH . $file2) )
       header("Location: " . url() . 'instalar.php?paso=2');
     else
@@ -81,7 +81,7 @@ switch( $paso ) {
       $post = "POST" == getenv('REQUEST_METHOD');
       if( $post ) {
         $test = new zerdb( @$_POST['host'], @$_POST['usuario'], @$_POST['clave'], @$_POST['db']);
-        if( $test && $test->listo ) {
+        if( $test && $test->ready ) {
           $data = file_get_contents($file);
           $reemplazar = array(
             "defineaquituhost" => trim($_POST['host']),
@@ -91,8 +91,8 @@ switch( $paso ) {
             );
           $actualizar = str_replace( array_keys($reemplazar), array_values($reemplazar), $data); // aquí el nuevo archivo (typ-config.php)
           if( !@fopen($file, "w") ) :
-            agregar_error( sprintf( __("No se puede abrir el archivo :/, por favor copia este archivo y reemplázalo por tu <b>typ-config-sample.php</b>
-              y cambia el nombre a <b>typ-config.php</b>") ) );
+            agregar_error( sprintf( __("No se puede abrir el archivo :/, por favor copia este archivo y reemplázalo por tu <strong>typ-config-sample.php</strong>
+              y cambia el nombre a <strong>typ-config.php</strong>") ) );
           echo '<br><br><textarea rows="25" readonly="readonly">' . $actualizar . '</textarea>';
           construir_pies(); exit();
           else:
@@ -101,16 +101,16 @@ switch( $paso ) {
             $renombrar = @rename( PATH . $file, PATH . $file2);
             fclose($f);
             if( ! $write ) :
-              echo agregar_error( __("No se puede reescribir el archivo :/, por favor copia este archivo y reemplázalo por tu <b>typ-config-sample.php</b> y cambia el nombre a <b>typ-config.php</b>, ó, si ya existe <b>typ-config.php</b>, actualizálo con esto:") . '<br><br><textarea rows="25" readonly="readonly">' . $actualizar . '</textarea>');
+              echo agregar_error( __("No se puede reescribir el archivo :/, por favor copia este archivo y reemplázalo por tu <strong>typ-config-sample.php</strong> y cambia el nombre a <strong>typ-config.php</strong>, ó, si ya existe <strong>typ-config.php</strong>, actualizálo con esto:") . '<br><br><textarea rows="25" readonly="readonly">' . $actualizar . '</textarea>');
               construir_pies() . exit(0);
             elseif( ! $renombrar ) :
               agregar_error( error_renombrar() );
-            else: 
+            else:
               exit( header("Location: instalar.php?paso=2") );
             endif;
           endif;
         }else{
-          agregar_error( sprintf( __("La conexión no está bien hecha... Error MySQL: %s "), mysql_error() ) );
+          agregar_error( sprintf( __("La conexión no está bien hecha... Error MySQL: %s "), $zerdb->error ) );
         }
       }
       ?>
@@ -159,41 +159,42 @@ switch( $paso ) {
     endif;
 else: // si existe typ-config.php
   require_once( './' . $file2 );
-  if( $zerdb->listo ) :
+  if( $zerdb->ready ) :
     exit( header("Location: instalar.php?paso=2") );
   else:
-    agregar_error( sprintf( __("<h2>Error estableciendo conexión con la base de datos</h2><p>Al parecer, no se puede conectar, por favor revisa tu <b>typ-config.php</b>, y he aquí el error devuelto por MySQL: <b>%s</b>"), $zerdb->ult_err) );
+    agregar_error( sprintf( __("<h2>Error estableciendo conexión con la base de datos</h2><p>Al parecer, no se puede conectar, por favor revisa tu <strong>typ-config.php</strong>, y he aquí el error devuelto por MySQL: <strong>%s</strong>"), $zerdb->ult_err) );
   endif;
 endif;
 break;
 case "2":
-if( ! ( file_exists('./typ-config.php') &&  $zerdb->listo) )
+if( ! ( file_exists('./typ-config.php') &&  $zerdb->ready) )
   exit( header("Location: instalar.php?paso=1") );
-if( !@new extraer($zerdb->usuarios, "*") ) 
+if( !$zerdb->select($zerdb->usuarios, "*")->_() )
   header("Location: instalar.php" );
 comp(); //here we're...
 /** TRAIGAN A LAS TABLAAAAAAAAAAAAAAS **/
 require_once( PATH . INC . 'esquema.php');
 foreach($sql as $a => $b)
   $zerdb->query($b); //inserta las tablas :3
+
 $post = 'POST' == getenv('REQUEST_METHOD');
 ?>
 <h2><?php _e("Bienvenido a la instalación") ?></h2>
 <hr>
 <?php if( ! $post ) { ?>
 <?php _e("Desde esta guía podrás instalar TrackYourPenguin fácilmente, sólo tienes que llenar los siguientes campos... :)") ?>
-<?php 
+<?php
 }else{
 
-  $usuario = @$zerdb->proteger( strtolower($_POST['usuario']) );
-  $clave = md5( @$zerdb->proteger($_POST['clave']) );
-  $email = @$zerdb->proteger( $_POST['email'] );
+  $usuario = @$zerdb->real_escape( strtolower($_POST['usuario']) );
+  $clave = md5( @$zerdb->real_escape($_POST['clave']) );
+  $email = @$zerdb->real_escape( $_POST['email'] );
 
   if( ! comprobar_args( @$_POST['usuario'], @$_POST['clave'], @$_POST['email'] ) ) {
     agregar_error( __("Haciendo trampa, ¿eh?") );
   }elseif( vacios( $_POST['usuario'], $_POST['clave'], $_POST['email'] ) ) {
     agregar_error( __("No puedes dejar datos vacíos") );
-  }elseif( ! preg_match('/^[a-z0-9_-\S]{3,12}$/', $usuario) ) {
+  }elseif( ! preg_match('/^[a-z0-9_-]{3,12}$/', $usuario) ) {
     agregar_error( __("El usuario no parece ser válido. Recuerda que debe ser de 3 a 12 caracteres.") );
   }elseif( strlen($_POST['email']) > 60) {
     agregar_error( __("¿Seguro que tienes un email tan largo?") );
@@ -204,10 +205,10 @@ $post = 'POST' == getenv('REQUEST_METHOD');
   }else{
   $path = dirname($_SERVER['PHP_SELF']) !== '/' ? dirname($_SERVER['PHP_SELF']) : '';
   $url = 'http://' . $_SERVER['HTTP_HOST'] . $path;
-  $insertar = $zerdb->insertar($zerdb->usuarios, array($usuario, $clave, $email, 1, 1, '') ) or die( $zerdb->ult_err );
-  $insertar2 = $zerdb->insertar($zerdb->config, array("TrackYourPenguin", $url, 1, json_encode( array("tema" => "bootstrap") ) ) ) or die( $zerdb->ult_err );
+  $insertar = $zerdb->insert($zerdb->usuarios, array($usuario, $clave, $email, 1, 1, '') ) or die( $zerdb->error );
+  $insertar2 = $zerdb->insert($zerdb->config, array("TrackYourPenguin", $url, json_encode( array("tema" => "bootstrap") ) ) ) or die( $zerdb->error );
   agregar_info( sprintf(
-    __('<h2> Instalación completada </h2><br> Has instalado TrackYourPenguin correctamente, ya puedes <a href="%s">iniciar sesión</a> para continuar. :)'), 
+    __('<h2> Instalación completada </h2><br> Has instalado TrackYourPenguin correctamente, ya puedes <a href="%s">iniciar sesión</a> para continuar. :)'),
   url() . 'acceso.php' ) );
   construir_pies();
   exit();
